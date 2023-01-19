b365d45a61110a53cc1554e6cebd2cb8d9732d9f
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2023-01-18 18:27:33 -0600

    adding bean validator to entity

--- src/main/java/com/spring/todo/api/todolistapi/entity/Item.java
+++ src/main/java/com/spring/todo/api/todolistapi/entity/Item.java
@@ -3,6 +3,9 @@ package com.spring.todo.api.todolistapi.entity;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+
 
 // This class is used to define the structure of the table in the database and the fields in the table
 // it also defines the arguments to each item in the table
@@ -17,9 +20,11 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    @NotNull(message = "Item cannot be null")
     @Column(name = "item")
     private String item;
 
+    @NotNull(message = "Status cannot be null")
     @Column(name = "status")
     private String status;
 
